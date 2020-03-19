04465db10d9718b2aa1fb50e4efca4f3119ed646
nicky <962943478@qq.com>
2020-03-19 17:37:45 +0800

    update

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,7 +1,41 @@
 <!--  -->
 <template>
-  <div>
+  <div class="search">
     <Header title="搜索" />
+    <form class="search_form" @submit.prevent="search">
+      <input
+        type="search"
+        placeholder="请输入商家名称"
+        class="search_input"
+        v-model="keyword"
+      />
+      <input type="submit" class="search_submit" />
+    </form>
+
+    <section class="list">
+      <ul class="list_container">
+        <router-link
+          :to="{ path: '/shop', query: { id: item.id } }"
+          tag="li"
+          v-for="(item, index) in 3"
+          :key="index"
+          class="list_li"
+        >
+          <section class="item_left">
+            <img :src="imgBaseUrl" class="restaurant_img" />
+          </section>
+          <section class="item_right">
+            <div class="item_right_text">
+              <p>
+                <span>{{ item.name }}</span>
+              </p>
+              <p>月售 10 单</p>
+              <p>23 元起送 / 距离33公里</p>
+            </div>
+          </section>
+        </router-link>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -9,7 +43,11 @@
 import Header from "../../components/header/header";
 export default {
   data() {
-    return {};
+    return {
+      keyword: null,
+      imgBaseUrl: "http://cangdu.org:8001/img/",
+      noSearchShops: false
+    };
   },
 
   components: {
@@ -20,7 +58,72 @@ export default {
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    search() {
+      console.log(this.keyword);
+    }
+  }
 };
 </script>
-<style lang="stylus" re="stylesheet/stylus"></style>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import "../../common/stylus/mixins.stylus"
+.search
+  width 100%
+  height 100%
+  overflow hidden
+  .search_form
+    clearFix()
+    margin-top 45px
+    background-color #fff
+    padding 12px 8px
+    input
+      height 35px
+      padding 0 4px
+      border-radius 2px
+      font-weight bold
+      outline none
+      &.search_input
+        float left
+        width 79%
+        border 4px solid #f2f2f2
+        font-size 14px
+        color #333
+        background-color #f2f2f2
+      &.search_submit
+        float right
+        width 18%
+        border 4px solid #02a774
+        font-size 16px
+        color #fff
+        background-color #02a774
+
+  .list
+    .list_container
+      background-color: #fff;
+      .list_li
+        display: flex;
+        justify-content: center;
+        padding: 10px
+        border-bottom: 1px solid $bc;
+        .item_left
+          margin-right: 10px
+          .restaurant_img
+            width 50px
+            height 50px
+            display block
+        .item_right
+          font-size 12px
+          flex 1
+          .item_right_text
+            p
+              line-height 12px
+              margin-bottom 6px
+              &:last-child
+                margin-bottom 0
+  .search_none
+    margin: 0 auto
+    color: #333
+    background-color: #fff
+    text-align: center
+    margin-top: 0.125rem
+</style>
