5a9d95bea1818bbeba7fbdb0fcc4138f22093d14
nicky <962943478@qq.com>
2020-03-20 18:06:22 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+import { Button } from "mint-ui";
 import App from "./App";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
+Vue.component(mtButton, Button); //<mt-button>
 /* eslint-disable no-new */
 new Vue({
     el: "#app",
