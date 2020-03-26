198c89a5fd7e20ed13f38226599e085db778dbda
nicky <962943478@qq.com>
2020-03-26 10:35:52 +0800

    update

--- src/pages/shop/rating/rating.vue
+++ src/pages/shop/rating/rating.vue
@@ -62,18 +62,10 @@
               </div>
               <p class="text">{{ r.text }}</p>
               <div class="recommend">
-                <span
-                  class="iconfont"
-                  :class="
+                <span class="iconfont" :class="
                     r.rateType === 0 ? 'icon-thumb_up' : 'icon-thumb_down'
-                  "
-                ></span>
-                <span
-                  class="item"
-                  v-for="(item, index) in r.recommend"
-                  :key="index"
-                  >{{ item }}</span
-                >
+                  "></span>
+                <span class="item" v-for="(item, index) in r.recommend" :key="index">{{ item }}</span>
               </div>
               <div class="time">{{ r.rateTime | date-format }}</div>
             </div>
@@ -89,7 +81,7 @@ import BScroll from "better-scroll";
 import { mapState, mapActions } from "vuex";
 import Star from "../../../components/star/star";
 export default {
-  data() {
+  data () {
     return {
       selectType: 2,
       onlyShowText: true
@@ -101,27 +93,39 @@ export default {
   computed: {
     ...mapState(["rating"]),
 
-    filterRating(){
-      let {rating,selectType,onlyShowText} = this
-      console.log(rating)
-      return rating.filter(item=>{
-        let {rateType, text} = item
-        return (selectType ==2 || selectType== rateType) && (text.length>0 || !onlyShowText)
+    filterRating () {
+      /*
+        条件1:
+            selectType: 0/1/2
+            rateType: 0/1
+            selectType===2 || selectType===rateType
+        条件2
+            onlyShowText: true/false
+            text: 有值/没值
+            !onlyShowText || text.length>0
+       */
+      let { rating, selectType, onlyShowText } = this
+      return rating.filter(item => {
+        // item.rateType==0 满意 this.selectType
+        // item.rateType==1 不满意 this.selectType
+        // item.rateType==2 全部 this.selectType
+        let { rateType, text } = item
+        return (selectType == 2 || selectType == rateType) && (text.length > 0 || !onlyShowText)
       })
-      
+
     }
   },
   methods: {
     ...mapActions(["getRatings"]),
-    toggleOnlyShowText(){
+    toggleOnlyShowText () {
       this.onlyShowText = !this.onlyShowText
     },
-    setSelectType(v){
-      this.selectType  = v
+    setSelectType (v) {
+      this.selectType = v
     }
   },
-  created() {},
-  mounted() {
+  created () { },
+  mounted () {
     this.getRatings(() => {
       this.$nextTick(() => {
         new BScroll(this.$refs.ratingsRef, {
@@ -134,182 +138,180 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-@import "../../../common/stylus/mixins.stylus"
-
+@import '../../../common/stylus/mixins.stylus'
 .ratings
-  position: absolute
-  top: 195px
-  bottom: 0
-  left: 0
-  width: 100%
-  overflow: hidden
-  background: #fff
+  position absolute
+  top 195px
+  bottom 0
+  left 0
+  width 100%
+  overflow hidden
+  background #fff
   .overview
-    display: flex
-    padding: 18px 0
+    display flex
+    padding 18px 0
     .overview-left
-      flex: 0 0 137px
-      padding: 6px 0
-      width: 137px
-      border-right: 1px solid rgba(7, 17, 27, 0.1)
-      text-align: center
+      flex 0 0 137px
+      padding 6px 0
+      width 137px
+      border-right 1px solid rgba(7, 17, 27, 0.1)
+      text-align center
       @media only screen and (max-width: 320px)
-        flex: 0 0 120px
-        width: 120px
+        flex 0 0 120px
+        width 120px
       .score
-        margin-bottom: 6px
-        line-height: 28px
-        font-size: 24px
-        color: rgb(255, 153, 0)
+        margin-bottom 6px
+        line-height 28px
+        font-size 24px
+        color rgb(255, 153, 0)
       .title
-        margin-bottom: 8px
-        line-height: 12px
-        font-size: 12px
-        color: rgb(7, 17, 27)
+        margin-bottom 8px
+        line-height 12px
+        font-size 12px
+        color rgb(7, 17, 27)
       .rank
-        line-height: 10px
-        font-size: 10px
-        color: rgb(147, 153, 159)
+        line-height 10px
+        font-size 10px
+        color rgb(147, 153, 159)
     .overview-right
-      flex: 1
-      padding: 6px 0 6px 24px
+      flex 1
+      padding 6px 0 6px 24px
       @media only screen and (max-width: 320px)
-        padding-left: 6px
+        padding-left 6px
       .score-wrapper
-        margin-bottom: 8px
-        font-size: 0
+        margin-bottom 8px
+        font-size 0
         .title
-          display: inline-block
-          line-height: 18px
-          vertical-align: top
-          font-size: 12px
-          color: rgb(7, 17, 27)
+          display inline-block
+          line-height 18px
+          vertical-align top
+          font-size 12px
+          color rgb(7, 17, 27)
         .star
-          display: inline-block
-          margin: 0 12px
-          vertical-align: top
+          display inline-block
+          margin 0 12px
+          vertical-align top
         .score
-          display: inline-block
-          line-height: 18px
-          vertical-align: top
-          font-size: 12px
-          color: rgb(255, 153, 0)
+          display inline-block
+          line-height 18px
+          vertical-align top
+          font-size 12px
+          color rgb(255, 153, 0)
       .delivery-wrapper
-        font-size: 0
+        font-size 0
         .title
-          line-height: 18px
-          font-size: 12px
-          color: rgb(7, 17, 27)
+          line-height 18px
+          font-size 12px
+          color rgb(7, 17, 27)
         .delivery
-          margin-left: 12px
-          font-size: 12px
-          color: rgb(147, 153, 159)
+          margin-left 12px
+          font-size 12px
+          color rgb(147, 153, 159)
   .split
-    width: 100%
-    height: 16px
-    border-top: 1px solid rgba(7, 17, 27, 0.1)
-    border-bottom: 1px solid rgba(7, 17, 27, 0.1)
-    background: #f3f5f7
+    width 100%
+    height 16px
+    border-top 1px solid rgba(7, 17, 27, 0.1)
+    border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    background #f3f5f7
   .ratingselect
     .rating-type
-      padding: 18px 0
-      margin: 0 18px
+      padding 18px 0
+      margin 0 18px
       border-1px(rgba(7, 17, 27, 0.1))
-      font-size: 0
+      font-size 0
       .block
-        display: inline-block
-        padding: 8px 12px
-        margin-right: 8px
-        line-height: 16px
-        border-radius: 1px
-        font-size: 12px
-        color: rgb(77, 85, 93)
-        background: rgba(77, 85, 93, 0.2)
+        display inline-block
+        padding 8px 12px
+        margin-right 8px
+        line-height 16px
+        border-radius 1px
+        font-size 12px
+        color rgb(77, 85, 93)
+        background rgba(77, 85, 93, 0.2)
         &.active
-          background: $green
-          color: #fff
+          background $green
+          color #fff
         .count
-          margin-left: 2px
-          font-size: 8px
+          margin-left 2px
+          font-size 8px
     .switch
-      padding: 12px 18px
-      line-height: 24px
-      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
-      color: rgb(147, 153, 159)
-      font-size: 0
+      padding 12px 18px
+      line-height 24px
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+      color rgb(147, 153, 159)
+      font-size 0
       &.on
         .icon-check_circle
-          color: $green
+          color $green
       .icon-check_circle
-        display: inline-block
-        vertical-align: top
-        margin-right: 4px
-        font-size: 24px
+        display inline-block
+        vertical-align top
+        margin-right 4px
+        font-size 24px
       .text
-        display: inline-block
-        vertical-align: top
-        font-size: 12px
+        display inline-block
+        vertical-align top
+        font-size 12px
   .rating-wrapper
-    padding: 0 18px
+    padding 0 18px
     .rating-item
-      display: flex
-      padding: 18px 0
+      display flex
+      padding 18px 0
       bottom-border-1px(rgba(7, 17, 27, 0.1))
       .avatar
-        flex: 0 0 28px
-        width: 28px
-        margin-right: 12px
+        flex 0 0 28px
+        width 28px
+        margin-right 12px
         img
-          border-radius: 50%
+          border-radius 50%
       .content
-        position: relative
-        flex: 1
+        position relative
+        flex 1
         .name
-          margin-bottom: 4px
-          line-height: 12px
-          font-size: 10px
-          color: rgb(7, 17, 27)
+          margin-bottom 4px
+          line-height 12px
+          font-size 10px
+          color rgb(7, 17, 27)
         .star-wrapper
-          margin-bottom: 6px
-          font-size: 0
+          margin-bottom 6px
+          font-size 0
           .star
-            display: inline-block
-            margin-right: 6px
-            vertical-align: top
+            display inline-block
+            margin-right 6px
+            vertical-align top
           .delivery
-            display: inline-block
-            vertical-align: top
-            line-height: 12px
-            font-size: 10px
-            color: rgb(147, 153, 159)
+            display inline-block
+            vertical-align top
+            line-height 12px
+            font-size 10px
+            color rgb(147, 153, 159)
         .text
-          margin-bottom: 8px
-          line-height: 18px
-          color: rgb(7, 17, 27)
-          font-size: 12px
+          margin-bottom 8px
+          line-height 18px
+          color rgb(7, 17, 27)
+          font-size 12px
         .recommend
-          line-height: 16px
-          font-size: 0
+          line-height 16px
+          font-size 0
           .icon-thumb_up, .icon-thumb_down, .item
-            display: inline-block
-            margin: 0 8px 4px 0
-            font-size: 9px
+            display inline-block
+            margin 0 8px 4px 0
+            font-size 9px
           .icon-thumb_up
-            color: $yellow
+            color $yellow
           .icon-thumb_down
-            color: rgb(147, 153, 159)
-
+            color rgb(147, 153, 159)
           .item
-            padding: 0 6px
-            border: 1px solid rgba(7, 17, 27, 0.1)
-            border-radius: 1px
-            color: rgb(147, 153, 159)
-            background: #fff
+            padding 0 6px
+            border 1px solid rgba(7, 17, 27, 0.1)
+            border-radius 1px
+            color rgb(147, 153, 159)
+            background #fff
         .time
-          position: absolute
-          top: 0
-          right: 0
-          line-height: 12px
-          font-size: 10px
-          color: rgb(147, 153, 159)
+          position absolute
+          top 0
+          right 0
+          line-height 12px
+          font-size 10px
+          color rgb(147, 153, 159)
 </style>
